f1167ab84bdf1b1317bd3b8ec1b0dc95e96f0c78
Eduard Galikhanov <godlik04@mail.ru>
2023-09-02 08:07:58 -0700

    screencastBlock compl

--- src/components/ScreencastBlock/ScreencastBlock.tsx
+++ src/components/ScreencastBlock/ScreencastBlock.tsx
@@ -5,12 +5,15 @@ import './screencastblock.sass'
 
 
 
+
 export const ScreencastBlock: FC<IScreencastBlock> = (props: IScreencastBlock) => {
 
   return (
-   <div className="screencastBlock">
-      ScreencastBlock
-   </div>
+    <div className="screencastBlock">
+      <iframe width="100%" height="386" src="https://www.youtube.com/embed/maPRR_jjyOE" title="JavaScript для начинающих 2023.
+       Полный курс за 6 часов. Уроки. Теория + практика" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+        gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+    </div>
   )
 }
 
