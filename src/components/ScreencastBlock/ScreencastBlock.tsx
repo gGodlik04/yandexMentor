067094b3a4d1c58200dcc43fdeafa34a748af371
Eduard Galikhanov <godlik04@mail.ru>
2023-09-05 06:44:24 -0700

    adaptive

--- src/components/ScreencastBlock/ScreencastBlock.tsx
+++ src/components/ScreencastBlock/ScreencastBlock.tsx
@@ -10,7 +10,7 @@ export const ScreencastBlock: FC<IScreencastBlock> = (/* props: IScreencastBlock
 
   return (
     <div className="screencastBlock">
-      <iframe width="100%" height="386" src="https://www.youtube.com/embed/LlHaPf4xpnM" title="CSS Анимация для SVG (For YandexMentor)" frameBorder="0" 
+      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/LlHaPf4xpnM" title="CSS Анимация для SVG (For YandexMentor)" frameBorder="0" 
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
     </div>
   )
